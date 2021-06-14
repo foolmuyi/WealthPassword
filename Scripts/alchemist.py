855b6d63856f28780a2d84843a3e6dc93df27df2
muyi <muyiorlk@gmail.com>
2021-06-14 23:46:13 +0800

    tester1

--- Scripts/alchemist.py
+++ Scripts/alchemist.py
@@ -3,6 +3,7 @@
 
 
 import os
+import time,datetime
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,86 +19,122 @@ def sharpeRatio(dailyChange):
 	return sharpe_ratio
 
 # 计算均值，默认五日算数均值
-# def ma(data,period=5,weights='default'):
-# 	if period <= 0:
-# 		raise IndexError('Period is '+str(period)+', which is less than 1')
-# 	elif len(data) < period:
-# 		raise IndexError('Cannot obtain MA because the length of the list is '+str(len(data))+' while the period is '+str(period))
-# 	elif (weights != 'default' and len(weights) != period):
-# 		raise IndexError('The length of weight list is '+str(len(weights))+' while the period is '+str(period))
-# 	else:
-# 		weights = [1 for _ in range(period)] if weights=='default' else weights
-# 		ma_list = [data[i] if i<period-1 else np.sum(np.array(data[i-period+1:i+1])*np.array(weights))/np.sum(weights) for i in range(len(data))]
-# 		return ma_list
+def ma(data,period=5,weights='default'):
+	if period <= 0:
+		raise IndexError('Period is '+str(period)+', which is less than 1')
+	elif len(data) < period:
+		raise IndexError('Cannot obtain MA because the length of the list is '+str(len(data))+' while the period is '+str(period))
+	elif (weights != 'default' and len(weights) != period):
+		raise IndexError('The length of weight list is '+str(len(weights))+' while the period is '+str(period))
+	else:
+		weights = [1 for _ in range(period)] if weights=='default' else weights
+		ma_list = [data[i] if i<period-1 else np.sum(np.array(data[i-period+1:i+1])*np.array(weights))/np.sum(weights) for i in range(len(data))]
+		return ma_list
+
+# 通过几只典型基金来计算所选时间段内开市天数
+def days_counter(data_dir,start_date,end_date):
+	test_fund_list = ['320007','000083','163406']    # 典型基金列表
+	days_sum = 0
+	try:
+		for each in test_fund_list:
+			file_path = data_dir+'/'+each+'.csv'
+			if os.path.exists(file_path):
+				df = pd.read_csv(file_path)
+				df['Date'] = pd.to_datetime(df['Date'])
+				df = df.set_index('Date')
+				df = df[start_date:end_date]
+				days_sum += df.shape[0]
+			else:
+				test_fund_list.remove(each)    # 若不存在则不考虑
+	except Exception as e:
+		pass
+	return days_sum/len(test_fund_list)
 
 # 按夏普比率进行筛选
-def filter_by_sharpe(data_dir, period=126, top=100):
+def filter_by_sharpe(data_dir, start_date='2021', end_date=None, top=100):
 	sharpe_ratio_list = {}
+	total_days = days_counter(data_dir,start_date,end_date)
 	file_list = os.listdir(data_dir)
 	for each in file_list:
 		file_path = os.path.join(data_dir,each)
 		df = pd.read_csv(file_path)
 		try:
-			df = df[-period:]
+			df['Date'] = pd.to_datetime(df['Date'])
+			df = df.set_index('Date')
+			df = df[start_date:end_date]
 			accWorth = df['AccWorth'].values.tolist()
 			dailyChange = df['Change'].values.tolist()
-			sharpe_ratio = sharpeRatio(dailyChange)
-			if (accWorth[-1]-accWorth[0])/accWorth[0] >= period/1000:    # 去掉收益率太低的基金
+			if len(dailyChange) < total_days-5:    # 去除成立时间太短和按周公布净值的基金(减5是为了留有一定的裕度)
+				sharpe_ratio = 0
+			else:
+				sharpe_ratio = sharpeRatio(dailyChange)
+			if (accWorth[-1]-accWorth[0])/accWorth[0] >= total_days/1000:    # 去掉收益率太低的基金
 				sharpe_ratio_list[each[:6]] = sharpe_ratio
 		except Exception as e:
 			pass
 	result = sorted(sharpe_ratio_list.items(), key = lambda x:x[1], reverse=True)[:top]    # 取前top名
 	return result
 
-data_dir = '../Data'
-half_year = filter_by_sharpe(data_dir)
-half_year_code = [each[0] for each in half_year]
-print(half_year)
-one_year = filter_by_sharpe(data_dir,252)
-one_year_code = [each[0] for each in one_year]
-print('\n')
-print(one_year)
-print('\n')
-print(list(set(half_year_code).intersection(set(one_year_code))))
+# 示例：筛选2021年至今和2020整年夏普比率均为前100的基金
+# data_dir = '../Data'
+# half_year = filter_by_sharpe(data_dir,'2021')
+# half_year_code = [each[0] for each in half_year]
+# print(half_year)
+# one_year = filter_by_sharpe(data_dir,'2020-01','2020-12')
+# one_year_code = [each[0] for each in one_year]
+# print('\n')
+# print(one_year)
+# print('\n')
+# print(list(set(half_year_code).intersection(set(one_year_code))))
 
 
-'''
-[('002149', 4.500442811169699), ('008099', 3.617632834975259), ('006448', 3.4437561927369074), ('001763', 3.3723080989059575), 
-('002943', 3.2185678333767105), ('001487', 3.11227566169346), ('005233', 3.0547716359310564), ('001681', 2.996398914095422),  
-('004235', 2.923452365633849), ('161222', 2.764638117278396), ('005613', 2.7406551935715937),
-('000049', 2.5938070073335213), ('162411', 2.5902233611914847), 
-('008763', 2.589581350083412), ('007844', 2.584233817715173), ('008764', 2.5827693621003336), ('005561', 2.5657550065240735), 
-('005562', 2.5593437939839494), ('007994', 2.5593163506610783), ('007995', 2.536153911086817), ('159980', 2.52522636788386), 
-('160416', 2.4993769986150633), ('160140', 2.483543917056399)]
-'''
+def getSignal(fund_code,start_date='2020',end_date=None):
+	file_path = '../Data/'+fund_code+'.csv'
+	df = pd.read_csv(file_path)
+	df['Date'] = pd.to_datetime(df['Date'])
+	df = df.set_index('Date')
+	df = df[start_date:end_date]
+	accWorth = df['AccWorth'].values.tolist()
+	dailyChange = df['Change'].values.tolist()
+	try:
+		ma5 = ma(accWorth,period=5,weights=[1,2,3,5,8])
+	except Exception as e:
+		return -1
+	new_ma5 = ma(ma5,period=5,weights=[1,2,3,5,8])
+	# plt.plot(accWorth,label='accWorth')
+	# plt.plot(ma5,label='MA5')
+	# plt.plot(new_ma5,label='New MA5')
+	# plt.legend()
+	# plt.show()
+	# slope = np.diff(new_ma5).tolist()
+	slope = np.gradient(new_ma5).tolist()
+	# sharpe_ratio = sharpeRatio(dailyChange)
+	if (slope[-1]>=0 and slope[-2]<0):
+		return 'Buy',accWorth
+	elif (slope[-1]<=0 and slope[-2]>0):
+		return 'Sell',accWorth
+	else:
+		return 'Hold',accWorth
+	# buy_spots = []
+	# sell_spots = []
+	# for i in range(len(slope)):
+	# 	if slope[i-1]>=0 and slope[i]<0:
+	# 		sell_spots.append(i)
+	# 	elif slope[i-1]<=0 and slope[i]>0:
+	# 		buy_spots.append(i)
+	# return buy_spots,sell_spots,accWorth,ma5,new_ma5
 
 
-# df = pd.read_csv('../Data/001316.csv')
-# dailyChange = df['Change'].values.tolist()[-250:]
-# ma5 = ma(accWorth,period=5,weights=[1,2,3,4,5])
-# ma10 = ma(accWorth,period=10,weights=[1,2,3,4,5,6,7,8,9,10])
-# fft_ma5 = np.fft.fft(accWorth)
-# filter_ma5 = np.where(np.abs(fft_ma5)<7,0,fft_ma5)
-# new_ma5 = np.real(np.fft.ifft(filter_ma5))
-# slope = np.diff(new_ma5).tolist()
-# slope[0:5] = [1.5,1.5,1.5,1.5,1.5]
-# slope[-5:] = [1.5,1.5,1.5,1.5,1.5]
-# buy_spots = []
-# sell_spots = []
-# for i in range(len(slope)):
-# 	if slope[i-1]>=0 and slope[i]<0:
-# 		sell_spots.append(i)
-# 	elif slope[i-1]<=0 and slope[i]>0:
-# 		buy_spots.append(i)
-
-
-
+# start_date = datetime.date(2020,1,2)
+# end_date = start_date+datetime.timedelta(days=365)
+# getSignal('320007',start_date,end_date)
 
 # plt.plot(accWorth,label='Acc Worth')
-# # plt.plot(ma5,label='MA5')
+# plt.plot(ma5,label='MA5')
+# plt.plot(new_ma5,label='New MA5')
 # # plt.plot(ma10,label='MA10')
 # # plt.plot(new_ma5,label='New MA5')
-# plt.vlines(buy_spots,1.5,3.25,color='g')
-# plt.vlines(sell_spots,1.5,3.25,color='r')
+# plt.vlines(buy_spots,min(accWorth),max(accWorth),color='g')
+# plt.vlines(sell_spots,min(accWorth),max(accWorth),color='r')
 # plt.legend()
 # plt.show()
--- Scripts/backtester.py
+++ Scripts/backtester.py
@@ -5,159 +5,49 @@
 import requests,json,os,time
 import pandas as pd
 import numpy as np
+import datetime as dt
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d
+import alchemist as am
 
 
-# class backTest():
+class backTester(object):
+	def __init__(self):
+		self.balance = 5000
+		self.myShares = 0
+		self.buy_date = []
+		self.sell_date = []
 
-# 初始化参数
-def initialize():
-	global balance,oneHand,oneInterval,buyDate,dateIndex,investAmount,myShares,balanceChange,buyInterval,sellThreshold
-	balance = 5000
-	oneHand = 100
-	oneInterval = 500
-	buyDate = '1970-1-1'
-	dateIndex = -10
-	investAmount = 0
-	myShares = 0
-	balanceChange = []
-	# 测试得到的阈值：连续上涨买入间隔时间、止盈收益率
-	buyInterval = 4
-	sellThreshold = 0.15
+	def Buy(self,accWorth,end_date,i):
+		if self.balance > 0:
+			self.myShares += self.balance/accWorth[-1]
+			print('Buy '+str(self.balance)+' on '+str(end_date))
+			self.balance = 0
+			self.buy_date.append(i)
 
+	def Sell(self,accWorth,end_date,i):
+		if self.myShares > 0:
+			self.balance += self.myShares*accWorth[-1]
+			print('Sell '+str(self.myShares*accWorth[-1])+' on '+str(end_date))
+			self.myShares = 0
+			self.sell_date.append(i)
 
-# 买入策略：本金不足时放慢买入速度
-def checkBuy(index, row):
-	global balance, oneHand, dateIndex, buyInterval, returnRate, investAmount, myShares
-	if balance < 0:
-		# print('Error occurred! Balance is less than 0!')
-		return
-	elif balance == 0:
-		return
-	elif index-dateIndex > buyInterval:
-		buyAmount = min(oneInterval, balance)
-		balance -= buyAmount
-		myShares += buyAmount/row['netWorth']
-		investAmount += buyAmount
-		# print('Buy '+str(int(buyAmount))+' on '+row['Date']+'. Balance:'+str(int(balance))+'. Total investment: '+str(int(investAmount)))
-		dateIndex = index
-		return
-	elif balance < 1000:
-		if (index-dateIndex > 3) & (row['Change'] <= -1):
-			buyAmount = min(int(np.abs(np.ceil(row['Change']))*oneHand), balance)
-			balance -= buyAmount
-			myShares += buyAmount/row['netWorth']
-			investAmount += buyAmount
-			# rint('Buy '+str(int(buyAmount))+' on '+row['Date']+'. Balance: '+str(int(balance))+'. Total investment: '+str(int(investAmount)))
-			dateIndex = index
-			return
-		else:
-			return
-	elif balance < 2000:
-		if (index-dateIndex > 2) & (row['Change'] <= -1):
-			buyAmount = min(int(np.abs(np.ceil(row['Change']))*oneHand), balance)
-			balance -= buyAmount
-			myShares += buyAmount/row['netWorth']
-			investAmount += buyAmount
-			# print('Buy '+str(int(buyAmount))+' on '+row['Date']+'. Balance: '+str(int(balance))+'. Total investment: '+str(int(investAmount)))
-			dateIndex = index
-			return
-		else:
-			return
-	else:
-		buyAmount = min(int(np.abs(np.ceil(row['Change']))*oneHand), balance)
-		balance -= buyAmount
-		myShares += buyAmount/row['netWorth']
-		investAmount += buyAmount
-		# print('Buy '+str(int(buyAmount))+' on '+row['Date']+'. Balance: '+str(int(balance))+'. Total investment: '+str(int(investAmount)))
-		dateIndex = index
-		return
+	def show_plot(self,accWorth):
+		plt.plot(accWorth)
+		plt.vlines(self.buy_date,1.5,3.5,color='g')
+		plt.vlines(self.sell_date,1.5,3.5,color='r')
+		plt.legend()
+		plt.show()
 
 
-# 卖出策略：达到止盈收益率后卖出一半
-def checkSell(row):
-	global balance, investAmount, myShares, balanceChange, sellThreshold
-	myCost = investAmount/myShares if myShares != 0 else 0
-	returnRate = (row['netWorth']-myCost)/myCost
-	# print(returnRate*100)
-	if (returnRate >= sellThreshold) & (myShares > 0):
-		shares2Sell = myShares/2
-		myShares -= shares2Sell
-		sellAmount = shares2Sell*row['netWorth']
-		balance += sellAmount
-		investAmount = myShares*row['netWorth']
-		# print('Sell '+str(int(sellAmount))+' on '+row['Date'])
-	totalAssest = balance+myShares*row['netWorth']
-	balanceChange.append(totalAssest)
-	# print(totalAssest)
+tester1 = backTester()
+start_date = dt.date(2019,1,2)
+for i in range(365,730):
+	end_date = start_date+dt.timedelta(days=i)
+	signal,accWorth = am.getSignal('320007',start_date,end_date)
+	if signal == 'Buy':
+		tester1.Buy(accWorth,end_date,i)
+	elif signal == 'Sell':
+		tester1.Sell(accWorth,end_date,i)
 
-# 单个基金回测函数
-def backTest(code):
-	global buyInterval, sellThreshold, balanceChange
-	data = pd.read_csv('./fundData/'+code+'.csv')
-	if len(data) < 500:
-		# print('Ignored newly established fund: '+code)
-		return 5000
-	else:
-		lastYear = data.iloc[-500:]
-	for index,row in lastYear.iterrows():
-		checkBuy(index, row)
-		checkSell(row)
-	return balanceChange[-1]
-
-
-
-# 测试寻找合适的参数：“连续上涨买入间隔天数”和“止盈收益率”
-# def backTest(code, buyInt, sellTh):
-# 	global buyInterval, sellThreshold, balanceChange
-# 	buyInterval = buyInt
-# 	sellThreshold = sellTh
-# 	# print('Interval: '+str(buyInterval)+'   Threshold: '+str(sellThreshold))
-# 	data = pd.read_csv('./fundData/'+code+'.csv')
-# 	last100days = data.iloc[-250:]
-# 	for index,row in last100days.iterrows():
-# 		checkBuy(index, row)
-# 		checkSell(row)
-# 	return balanceChange[-1]
-
-# finalRes = 0
-# finalResList = []
-# my_list = ['162605','020005','000529','005267','001217','006253','161818','005969','005534','007043','002846','005236','000263','004868','570008','005669','001385','001382','001832','006616']
-# for buyInt in [4,5,6,7,8,9,10]:
-# 	for sellTh in [0.05,0.1,0.15,0.2,0.25,0.3]:
-# 		for code in my_list:
-# 			initialize()
-# 			oneRes = backTest(code, buyInt, sellTh)
-# 			finalRes += oneRes
-# 		finalRate = (finalRes - 20*5000)/(20*5000)
-# 		finalResList.append(finalRes)
-# 		print('Interval: '+str(buyInt)+'   Threshold: '+str(sellTh)+'    Return rate: '+str(finalRate))
-# 		finalRes = 0
-
-# xx,yy = np.meshgrid([4,5,6,7,8,9,10],[0.05,0.1,0.15,0.2,0.25,0.3])
-# zz = np.array(finalResList)
-# zz.shape = (7,6)
-# zz = np.transpose(zz)
-# ax1 = plt.axes(projection='3d')
-# ax1.plot_surface(xx,yy,zz,cmap='rainbow')
-# plt.show()
-
-
-
-
-# fundFileList = os.listdir('./fundData')
-# fundCodeList = []
-# for file in fundFileList:
-# 	code = file[:-4]
-# 	fundCodeList.append(code)
-# for code in fundCodeList:
-# 	initialize()
-# 	result = backTest(code)
-# 	if result > 12500:
-# 		print('code: '+code+'   amount: '+str(result))
-
-# initialize()
-# backTest('006679')
-# plt.plot(balanceChange)
-# plt.show()
+tester1.show_plot(accWorth)
