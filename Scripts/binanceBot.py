ec104ed9a0383ef216c392d43c44d13992133d47
muyi <muyiorlk@gmail.com>
2021-08-14 21:13:57 +0800

    support trialing stop

--- Scripts/binanceBot.py
+++ Scripts/binanceBot.py
@@ -4,11 +4,9 @@ import json,time
 import requests
 import hmac,hashlib
 import numpy as np
+import traceback
 import smtplib
 from email.mime.text import MIMEText
-
-
-# This script runs every 15 minutes
 
 
 # calcute ema
@@ -68,13 +66,38 @@ def getAccountInfo(API_key,API_secret):
 	res = requests.get(url=info_url,headers={'X-MBX-APIKEY':API_key},params=query_string)
 	return res
 
-# acquire marker price
-def getMakerPrice(API_key,API_secret):
-	info_url = 'https://fapi.binance.com//fapi/v1/ticker/bookTicker'
+# acquire account balance
+def getBalance(API_key,API_secret,asset):
+	info_url = 'https://fapi.binance.com/fapi/v2/balance'
 	timestamp = int(time.time()*1000)
+	query_string = {'timestamp':timestamp}
+	query_string['signature'] = getSig(API_secret,param2string(query_string))
+	res = requests.get(url=info_url,headers={'X-MBX-APIKEY':API_key},params=query_string)
+	balance_raw = json.loads(res.text)
+	asset_balance_info = [each for each in balance_raw if each['asset'] == asset][0]
+	asset_balance = float(asset_balance_info['availableBalance'])
+	return asset_balance
+
+# acquire mark price
+def getMarkPrice(API_key,API_secret,symbol):
+	info_url = 'https://fapi.binance.com/fapi/v1/premiumIndex'
+	query_string = {'symbol':symbol}
+	res = requests.get(url=info_url,params=query_string)
+	mark_price_raw = json.loads(res.text)
+	mark_price = float(mark_price_raw['markPrice'])
+	return mark_price
+
+# acquire maker price
+def getMakerPrice(API_key,API_secret,side):
+	info_url = 'https://fapi.binance.com//fapi/v1/ticker/bookTicker'
 	query_string = {'symbol':'ETHBUSD'}
 	res = requests.get(url=info_url,params=query_string)
-	return res
+	maker_price_info = json.loads(res.text)
+	if side == 'BUY':
+		maker_price = float(maker_price_info['bidPrice'])
+	elif side == 'SELL':
+		maker_price = float(maker_price_info['askPrice'])
+	return maker_price
 
 # order at market price (stop loss mainly)
 def order(API_key,API_secret,side,quantity):
@@ -85,16 +108,11 @@ def order(API_key,API_secret,side,quantity):
 	res = requests.post(url=order_url,headers={'X-MBX-APIKEY':API_key},data=query_string)
 	return res
 
-# order as marker (open positions)
+# order as maker (open positions)
 def order_maker(API_key,API_secret,side,quantity):
 	order_url = 'https://fapi.binance.com/fapi/v1/order'
 	timestamp = int(time.time()*1000)
-	maker_price_raw = getMakerPrice(API_key,API_secret)
-	maker_price_info = json.loads(maker_price_raw.text)
-	if side == 'BUY':
-		maker_price = float(maker_price_info['bidPrice'])
-	elif side == 'SELL':
-		maker_price = float(maker_price_info['askPrice'])
+	maker_price = getMakerPrice(API_key,API_secret,side)
 	query_string = {'symbol':'ETHBUSD','side':side,'type':'LIMIT','quantity':quantity,'price':maker_price,'timeInForce':'GTX','timestamp':timestamp}
 	query_string['signature'] = getSig(API_secret,param2string(query_string))
 	res = requests.post(url=order_url,headers={'X-MBX-APIKEY':API_key},data=query_string)
@@ -107,7 +125,8 @@ def getOpenOrders(API_key,API_secret):
 	query_string = {'symbol':'ETHBUSD','timestamp':timestamp}
 	query_string['signature'] = getSig(API_secret,param2string(query_string))
 	res = requests.get(url=info_url,headers={'X-MBX-APIKEY':API_key},params=query_string)
-	return res
+	open_orders = json.loads(res.text)
+	return open_orders
 
 # close current open order
 def closeOpenOrders(API_key,API_secret):
@@ -118,6 +137,7 @@ def closeOpenOrders(API_key,API_secret):
 	res = requests.delete(url=info_url,headers={'X-MBX-APIKEY':API_key},params=query_string)
 	return res
 
+# get position and entry price
 def getPositionAndPrice(API_key,API_secret):
 	account_info_res = getAccountInfo(API_key,API_secret)
 	account_info = json.loads(account_info_res.text)
@@ -126,12 +146,57 @@ def getPositionAndPrice(API_key,API_secret):
 	entry_price = float(ETHBUSD_info['entryPrice'])
 	return positionAmt,entry_price
 
+# calculate order quantity
+def getMyQuantity(API_key,API_secret,leverage):
+	balance = getBalance(API_key,API_secret,'BUSD')
+	mark_price = getMarkPrice(API_key,API_secret,'ETHBUSD')
+	quantity = round(balance*leverage/3/mark_price,3)
+	return quantity
+
+# try to order as maker for 10 times
+def orderMaker10Times(API_key,API_secret,side,quantity):
+	closeOpenOrders(API_key,API_secret)
+	# initial position amount. 0 when open new position, real position when take profit
+	initPositionAmt, _ = getPositionAndPrice(API_key,API_secret)
+	positionAmt, _ = getPositionAndPrice(API_key,API_secret)
+	count = 1
+	while (positionAmt == initPositionAmt) and (count <= 10):
+		order_maker(API_key,API_secret,side,quantity)
+		time.sleep(3)
+		closeOpenOrders(API_key,API_secret)
+		positionAmt, _ = getPositionAndPrice(API_key,API_secret)
+		count += 1
+
+# calculate current ROE
+def calcROE(API_key,API_secret,symbol='ETHBUSD'):
+	positionAmt, entry_price = getPositionAndPrice(API_key,API_secret)
+	mark_price = getMarkPrice(API_key,API_secret,symbol)
+	if positionAmt > 0:
+		ROE = round((mark_price-entry_price)/entry_price*100,2)
+	elif positionAmt < 0:
+		ROE = round((entry_price-mark_price)/entry_price*100,2)
+	else:
+		ROE = 0
+	return ROE
+
+# write stopROE value to file
+def writeStopROE(stopROE,path='/home/username/BinanceBot/stopROE.json'):
+	with open(path,'w') as f:
+		f.write(str(stopROE))
+
+# read stopROE value from file
+def getStopROE(path='/home/username/BinanceBot/stopROE.json'):
+	with open(path,'r') as f:
+		stopROE = json.load(f)
+	return stopROE
+
+
 # send email to me when error occures
 def sendMail(mail_content,subject='Error Occurred!'):
 	mail_host = 'smtp.163.com'
 	mail_username = 'example@163.com'
-	mail_pw = 'passwordexample'
-	mail_recv = ['example@xxx.com']
+	mail_pw = 'EMAILPASSWORD'
+	mail_recv = ['example@example.com']
 	message = MIMEText(mail_content,'plain','utf-8')
 	message['Subject'] = subject
 	message['From'] = 'example@163.com'
@@ -141,61 +206,98 @@ def sendMail(mail_content,subject='Error Occurred!'):
 	smtpObj.sendmail(mail_username,mail_recv[0],message.as_string())
 	smtpObj.quit()
 
+
 # main function
 def run():
 	API_key = 'APIKEYEXAMPLE'
 	API_secret = 'APISECRETEXAMPLE'
 	fast_factor = 3
 	slow_factor = 6
-	quantity = 0.04
+	leverage = 10
 	# setLeverage(API_key,API_secret)    # for test
 	positionAmt, entry_price = getPositionAndPrice(API_key,API_secret)
-	close_price = getData()[:-1]    # delete the last price because the last one is current price
+	close_price = getData()[:-1]    # remove the last data point because it is current price
 	ema_fast = ema(close_price,fast_factor)
 	ema_slow = ema(close_price,slow_factor)
 
 	# close open orders
-	open_orders = json.loads(getOpenOrders(API_key,API_secret).text)
-	if open_orders != []:
-		closeOpenOrders(API_key,API_secret)
+	closeOpenOrders(API_key,API_secret)
+
+	# close position to stop loss (trend reverse situation)
+	if (positionAmt > 0) and (ema_fast[-1] < ema_slow[-1]):    # trend reversed, close long position
+		order(API_key,API_secret,'SELL',positionAmt)    # close position
+		stopROE = -0.8    # reset stopROE after close position
+		time.sleep(1)    # wait close position order to complete
+	elif (positionAmt < 0) and (ema_fast[-1] > ema_slow[-1]):    # trend reversed, close short position
+		order(API_key,API_secret,'BUY',-positionAmt)    # close position
+		stopROE = -0.8    # reset stopROE after close position
+		time.sleep(1)    # wait close position order to complete
 	else:
 		pass
 
-	# # 平仓
-	if positionAmt > 0:
-		if ema_fast[-1] < ema_slow[-1]:    # 趋势反转，平多
-			order(API_key,API_secret,'SELL',positionAmt)
-		elif (close_price[-1]-entry_price)/entry_price < -0.008:    # 止损
-			order(API_key,API_secret,'SELL',positionAmt)
-		elif (close_price[-1]-entry_price)/entry_price > 0.008:    # 止盈
-			order_maker(API_key,API_secret,'SELL',positionAmt)
-	elif positionAmt < 0:
-		if ema_fast[-1] > ema_slow[-1]:    # 趋势反转，平空
-			order(API_key,API_secret,'BUY',-positionAmt)
-		elif (close_price[-1]-entry_price)/entry_price < -0.008:    # 止盈
-			order_maker(API_key,API_secret,'BUY',-positionAmt)
-		elif (close_price[-1]-entry_price)/entry_price > 0.008:    # 止损
-			order(API_key,API_secret,'BUY',-positionAmt)
-	else:
-		pass
-
-	# wait maker order to complete
-	time.sleep(5)
+	# trailing stop
+	positionAmt, entry_price = getPositionAndPrice(API_key,API_secret)
+	stopROE = getStopROE()
+	if positionAmt != 0:
+		ROE = calcROE(API_key,API_secret)
+		if ROE < stopROE:    # close position
+			if positionAmt > 0:
+				orderMaker10Times(API_key,API_secret,'SELL',positionAmt)
+				stopROE = -0.8    # reset stopROE after close position
+			elif positionAmt < 0:
+				orderMaker10Times(API_key,API_secret,'BUY',-positionAmt)
+				stopROE = -0.8    # reset stopROE after close position
+			else:
+				pass
+		elif ROE > 11.5:
+			stopROE = max(10.3,stopROE)
+		elif ROE > 10.3:
+			stopROE = max(9.2,stopROE)
+		elif ROE > 9.2:
+			stopROE = max(8.1,stopROE)
+		elif ROE > 8.1:
+			stopROE = max(7.1,stopROE)
+		elif ROE > 7.1:
+			stopROE = max(6.1,stopROE)
+		elif ROE > 6.1:
+			stopROE = max(5.2,stopROE)
+		elif ROE > 5.2:
+			stopROE = max(4.3,stopROE)
+		elif ROE > 4.3:
+			stopROE = max(3.5,stopROE)
+		elif ROE > 3.5:
+			stopROE = max(2.7,stopROE)
+		elif ROE > 2.7:
+			stopROE = max(2,stopROE)
+		elif ROE > 2:
+			stopROE = max(1.3,stopROE)
+		elif ROE > 1.3:
+			stopROE = max(0.7,stopROE)
+		elif ROE > 0.7:
+			stopROE = max(0.1,stopROE)
 	
 	# check current position again before open position
 	positionAmt, _ = getPositionAndPrice(API_key,API_secret)
 
-	# 建仓
+	# get quantity to open new order
+	quantity = getMyQuantity(API_key,API_secret,leverage)
+
+	# open position
 	if (ema_fast[-2] < ema_slow[-2]) and (ema_fast[-1] > ema_slow[-1]) and (positionAmt == 0):
-		order(API_key,API_secret,'BUY',quantity)
+		orderMaker10Times(API_key,API_secret,'BUY',quantity)
+		stopROE = -0.8
 	elif (ema_fast[-2] > ema_slow[-2]) and (ema_fast[-1] < ema_slow[-1]) and (positionAmt == 0):
-		order(API_key,API_secret,'SELL',quantity)
+		orderMaker10Times(API_key,API_secret,'SELL',quantity)
+		stopROE = -0.8
 	else:
 		pass
+
+	# save stopROE value to file
+	writeStopROE(stopROE)
 
 
 if __name__ == "__main__":
 	try:
 		run()
 	except Exception as e:
-		sendMail(str(e))
+		sendMail(traceback.format_exc())
--- Scripts/binanceTest_2ema.py
+++ Scripts/binanceTest_2ema.py
@@ -51,36 +51,32 @@ def calc_slope(data_list):
 	return k
 
 
-columns = ['OpenTime','Open','High','Low','Close']
-EU_1m_1 = pd.read_csv('../Binance/ETHUSDT-1m-2021-04.csv').iloc[:,0:5]
-EU_1m_2 = pd.read_csv('../Binance/ETHUSDT-1m-2021-05.csv').iloc[:,0:5]
-EU_1m_3 = pd.read_csv('../Binance/ETHUSDT-1m-2021-06.csv').iloc[:,0:5]
-EU_1m_1.columns = columns
-EU_1m_2.columns = columns
-EU_1m_3.columns = columns
-EU_1m = pd.concat([EU_1m_1,EU_1m_2,EU_1m_3],ignore_index=True)
-close_price = EU_1m['Close'].values.tolist()
-open_price = EU_1m['Open'].values.tolist()
-high_price = EU_1m['High'].values.tolist()
-low_price = EU_1m['Low'].values.tolist()
 
 columns = ['OpenTime','Open','High','Low','Close']
-EU_15m_1 = pd.read_csv('../Binance/ETHUSDT-15m-2021-04.csv').iloc[:,0:5]
-EU_15m_2 = pd.read_csv('../Binance/ETHUSDT-15m-2021-05.csv').iloc[:,0:5]
-EU_15m_3 = pd.read_csv('../Binance/ETHUSDT-15m-2021-06.csv').iloc[:,0:5]
-EU_15m_1.columns = columns
-EU_15m_2.columns = columns
-EU_15m_3.columns = columns
-EU_15m = pd.concat([EU_15m_1,EU_15m_2,EU_15m_3],ignore_index=True)
-close_price_15m = EU_15m['Close'].values.tolist()
-
+EU_15m_12 = pd.read_csv('../Binance/ETHUSDT-15m-2020-12.csv').iloc[:,0:5]
+EU_15m_01 = pd.read_csv('../Binance/ETHUSDT-15m-2021-01.csv').iloc[:,0:5]
+EU_15m_02 = pd.read_csv('../Binance/ETHUSDT-15m-2021-02.csv').iloc[:,0:5]
+EU_15m_03 = pd.read_csv('../Binance/ETHUSDT-15m-2021-03.csv').iloc[:,0:5]
+EU_15m_04 = pd.read_csv('../Binance/ETHUSDT-15m-2021-04.csv').iloc[:,0:5]
+EU_15m_05 = pd.read_csv('../Binance/ETHUSDT-15m-2021-05.csv').iloc[:,0:5]
+EU_15m_12.columns = columns
+EU_15m_01.columns = columns
+EU_15m_02.columns = columns
+EU_15m_03.columns = columns
+EU_15m_04.columns = columns
+EU_15m_05.columns = columns
+EU_15m_6m = pd.concat([EU_15m_12,EU_15m_01,EU_15m_02,EU_15m_03,EU_15m_04,EU_15m_05],ignore_index=True)
+close_price = EU_15m_6m['Close'].values.tolist()
+open_price = EU_15m_6m['Open'].values.tolist()
+high_price = EU_15m_6m['High'].values.tolist()
+low_price = EU_15m_6m['Low'].values.tolist()
 
 class Backtester(object):
 
 	def __init__(self,fast_param,slow_param):
 		self.fast_param = fast_param
 		self.slow_param = slow_param
-		self.balance = 10
+		self.balance = 100
 		self.totalBalance = [self.balance]    # 收益曲线
 		self.myShare = 0
 		self.state = 'sleeping'
@@ -101,8 +97,8 @@ class Backtester(object):
 	def closeLong(self,i):
 		self.state = 'sleeping'
 		self.close_long_price = close_price[i-1]
-		# profit = (self.close_long_price-self.buy_price)*self.myShare-self.buy_price*self.myShare*0.00007
-		profit = (self.close_long_price-self.buy_price)*self.myShare
+		profit = (self.close_long_price-self.buy_price)*self.myShare-self.buy_price*self.myShare*0.00007
+		# profit = (self.close_long_price-self.buy_price)*self.myShare
 		self.balance = self.buy_price*self.myShare+profit
 		self.totalBalance.append(self.balance)    # 收益曲线统计
 		self.myShare = 0
@@ -124,8 +120,8 @@ class Backtester(object):
 	def closeShort(self,i):
 		self.state = 'sleeping'
 		self.close_short_price = close_price[i-1]
-		# profit = (self.sell_price-self.close_short_price)*self.myShare-self.sell_price*self.myShare*0.00007
-		profit = (self.sell_price-self.close_short_price)*self.myShare
+		profit = (self.sell_price-self.close_short_price)*self.myShare-self.sell_price*self.myShare*0.00007
+		# profit = (self.sell_price-self.close_short_price)*self.myShare
 		self.balance = self.sell_price*self.myShare+profit
 		self.totalBalance.append(self.balance)    # 收益曲线统计
 		self.myShare = 0
@@ -136,74 +132,33 @@ class Backtester(object):
 		print('平空: '+str(self.close_short_price)+'  profit: '+str(profit))
 		self.close_points.append(i)
 
-	def calcROE(self,now_price):
-		if self.state == 'bought':
-			ROE = (now_price-self.buy_price)/self.buy_price*100
-		elif self.state == 'sold':
-			ROE = (self.sell_price-now_price)/self.sell_price*100
-		else:
-			ROE = 0
-		return ROE
-
 	def test(self):
-		for i in range(1500*15,len(close_price)):
-			k = i//15
-			new_close_price = close_price_15m[k-200:k]
-			ema_fast = ema(new_close_price,self.fast_param)
-			ema_slow = ema(new_close_price,self.slow_param)
+		for i in range(self.slow_param,len(close_price)):
+			ema_fast = ema(close_price[:i],self.fast_param)
+			ema_slow = ema(close_price[:i],self.slow_param)
 
-			if self.state != 'sleeping':
-				if self.state == 'bought':
-					if ema_fast[-1] < ema_slow[-1]:
-						self.closeLong(i)
-				elif self.state == 'sold':
-					if ema_fast[-1] > ema_slow[-1]:
-						self.closeShort(i)
-				else:
-					pass
-				ROE = self.calcROE(close_price[i-1])
-				if ROE < stopROE:
-					if self.state == 'bought':
-						self.closeLong(i)
-					elif self.state == 'sold':
-						self.closeShort(i)
-					else:
-						pass
-				elif ROE > 11.5:
-					stopROE = max(10.3,stopROE)
-				elif ROE > 10.3:
-					stopROE = max(9.2,stopROE)
-				elif ROE > 9.2:
-					stopROE = max(8.1,stopROE)
-				elif ROE > 8.1:
-					stopROE = max(7.1,stopROE)
-				elif ROE > 7.1:
-					stopROE = max(6.1,stopROE)
-				elif ROE > 6.1:
-					stopROE = max(5.2,stopROE)
-				elif ROE > 5.2:
-					stopROE = max(4.3,stopROE)
-				elif ROE > 4.3:
-					stopROE = max(3.5,stopROE)
-				elif ROE > 3.5:
-					stopROE = max(2.7,stopROE)
-				elif ROE > 2.7:
-					stopROE = max(2,stopROE)
-				elif ROE > 2:
-					stopROE = max(1.3,stopROE)
-				elif ROE > 1.3:
-					stopROE = max(0.7,stopROE)
-				elif ROE > 0.7:
-					stopROE = max(0.1,stopROE)
-				
+			if self.state == 'bought':
+				if ema_fast[-1] < ema_slow[-1]:
+					self.closeLong(i)
+				elif (close_price[i-1]-self.buy_price)/self.buy_price < -0.008:
+					self.closeLong(i) 
+				elif (close_price[i-1]-self.buy_price)/self.buy_price > 0.008:
+					self.closeLong(i)
+			elif self.state == 'sold':
+				if ema_fast[-1] > ema_slow[-1]:
+					self.closeShort(i)
+				elif (close_price[i-1]-self.sell_price)/self.sell_price < -0.008:
+					self.closeShort(i)
+				elif (close_price[i-1]-self.sell_price)/self.sell_price > 0.008:
+					self.closeShort(i)
+			else:
+				pass
 
 			# 建仓
 			if (ema_fast[-2] < ema_slow[-2]) and (ema_fast[-1] > ema_slow[-1]) and (self.state == 'sleeping'):
 				self.buyLong(i)
-				stopROE = -0.8
 			elif (ema_fast[-2] > ema_slow[-2]) and (ema_fast[-1] < ema_slow[-1]) and (self.state == 'sleeping'):
 				self.sellShort(i)
-				stopROE = -0.8
 			else:
 				pass
 
@@ -237,17 +192,36 @@ class Backtester(object):
 		plt.show()
 
 
-tester = Backtester(3,6)
+tester = Backtester(6,8)
 tester.test()
 tester.show_plot()
+
+
+# EMA3+EMA9: 564.8672209577791    平均交易间隔: 2.147117296222664    胜率: 0.4181999005469915
+# EMA3+EMA5: 472.7846147953333    平均交易间隔: 1.5806805708013172    胜率: 0.39824304538799415
+# EMA3+EMA11: 453.4176318318149    平均交易间隔: 2.369720241360395    胜率: 0.41822173435784854
+# EMA3+EMA7: 597.6206526493878    平均交易间隔: 1.9064430714916152    胜率: 0.41589403973509936
+# EMA4+EMA6: 427.61956611921426    平均交易间隔: 2.036775106082037    胜率: 0.4235849056603774
+# EMA4+EMA12: 283.1587267867749    平均交易间隔: 2.810670136629798    胜率: 0.427734375
+# EMA4+EMA10: 355.6210244593381    平均交易间隔: 2.545668827342369    胜率: 0.4251179245283019
+# EMA4+EMA8: 540.9145682969227    平均交易间隔: 2.3684210526315788    胜率: 0.43828853538123974
+# EMA5+EMA7: 503.1477689919874    平均交易间隔: 2.478485370051635    胜率: 0.4397244546498278
+# EMA5+EMA9: 343.77429467127564    平均交易间隔: 2.7    胜率: 0.4371482176360225
+# EMA5+EMA13: 276.4214943158934    平均交易间隔: 3.3179723502304146    胜率: 0.4465795541890853
+# EMA5+EMA11: 280.7951764477952    平均交易间隔: 3.0273300630693765    胜率: 0.4375876577840112
+# EMA6+EMA8: 383.99094776829105    平均交易间隔: 2.89738430583501    胜率: 0.44966442953020136
+# EMA6+EMA10: 223.97894981278958    平均交易间隔: 3.1555880204528854    胜率: 0.43932748538011696
+# EMA6+EMA12: 221.1261677293512    平均交易间隔: 3.486682808716707    胜率: 0.4491114701130856
+# EMA6+EMA14: 184.13526321260022    平均交易间隔: 3.6986301369863015    胜率: 0.4490145672664953
 
 
 # 寻找最佳参数组合
 # pool = multiprocessing.Pool(6)
 # manager = multiprocessing.Manager()
 # param_comb = manager.dict()
-# for trend_param in range(10,200,5):
-# 	pool.apply_async(Backtester(3,6).test,(trend_param,))
+# for fast_param in range(3,7):
+# 	for slow_param in range(fast_param,fast_param+10,2):
+# 		pool.apply_async(Backtester(fast_param,slow_param).test,)
 
 # pool.close()
 # pool.join()
