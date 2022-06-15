06e7dec3a0539c6b0aac4c34648df048cb9fb515
muyi <muyiorlk@gmail.com>
2022-06-16 03:26:40 +0800

    update strategy

--- Scripts/backtraderTest_A.py
+++ Scripts/backtraderTest_A.py
@@ -18,7 +18,7 @@ import multiprocessing
 
 
 def get_data(code, start_date, end_date):
-    hist_data = ak.stock_zh_a_hist(symbol=code, period='daily', start_date=start_date, end_date=end_date, adjust='hfq')
+    hist_data = ak.stock_zh_a_hist(symbol=code, period='daily', start_date=start_date, end_date=end_date, adjust='qfq')
     df = hist_data.iloc[:, 0:6]
     df.columns = ['datetime', 'open', 'close', 'high', 'low', 'volume']
     df.index = pd.to_datetime(df.datetime)
@@ -79,6 +79,74 @@ class SimpleStrategy(bt.Strategy):
         # self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' % (trade.pnl, trade.pnlcomm))
 
     # 检查均线发散条件
+    def checkDiv(self):
+        fast_minus_month = pd.Series(self.ma_fast.get(size=5)) - pd.Series(self.ma_month.get(size=5))
+        cond_1 = fast_minus_month.equals(fast_minus_month.sort_values())
+        month_minus_quarter = pd.Series(self.ma_month.get(size=5)) - pd.Series(self.ma_quarter.get(size=5))
+        cond_2 = month_minus_quarter.equals(month_minus_quarter.sort_values())
+        quarter_minus_year = pd.Series(self.ma_quarter.get(size=5)) - pd.Series(self.ma_year.get(size=5))
+        cond_3 = quarter_minus_year.equals(quarter_minus_year.sort_values())
+        if (cond_1 and cond_2 and cond_3):
+            return True
+        else:
+            return False
+
+    # 检查均线发散趋势是否改变，用于判断顶部
+    def checkNotDiv(self):
+        fast_minus_month = pd.Series(self.ma_fast.get(size=6)) - pd.Series(self.ma_month.get(size=6))
+        cond_1 = not fast_minus_month.equals(fast_minus_month.sort_values())
+        month_minus_quarter = pd.Series(self.ma_month.get(size=6)) - pd.Series(self.ma_quarter.get(size=6))
+        cond_2 = not month_minus_quarter.equals(month_minus_quarter.sort_values())
+        quarter_minus_year = pd.Series(self.ma_quarter.get(size=6)) - pd.Series(self.ma_year.get(size=6))
+        cond_3 = not quarter_minus_year.equals(quarter_minus_year.sort_values())
+        if (cond_1 or cond_2 or cond_3):
+            return True
+        else:
+            return False
+
+    # 从当前点向前寻找趋势首日
+    def findDDay(self):
+        for i in range(len(self) - self.params.year_param + 1):    # 从第self.params.year_param日开始才有年线
+            if (self.ma_fast[-i] > self.ma_month[-i] > self.ma_quarter[-i]):
+                continue
+            # cond_1 = (self.ma_month[-i] - self.ma_quarter[-i]) > (self.ma_month[-i-1] - self.ma_quarter[-i-1])
+            # cond_2 = (self.ma_quarter[-i] - self.ma_year[-i]) > (self.ma_quarter[-i-1] - self.ma_year[-i-1])
+            # if (self.ma_month[-i] < self.ma_quarter[-i]):    # 月线上穿季度线，不再向前找
+            #     break
+            # elif cond_1 and cond_2:
+            #     continue
+            else:
+                break
+        return i
+
+    # 计算本次回撤是趋势形成以来的第几次回撤
+    def countDrawdowns(self, days):
+        drawdowns = 0
+        if days <= 5:
+            drawdowns = 0
+        else:
+            for i in range(days-5):
+                cond_1 = self.data.close[-i] < self.ma_fast[-i]    # 当日收盘低于fast线
+                cond_2 = min(pd.Series(self.data.close.get(ago=-i-1, size=5)) - pd.Series(self.ma_fast.get(ago=-i-1, size=5))) >= 0    # 前5日收盘均高于fast线
+                if cond_1 and cond_2:
+                    drawdowns += 1
+        return drawdowns
+
+    # 计算本次回撤是趋势形成以来的第几次回撤
+    def findDrawdownDay(self, days):
+        ago_days = 0
+        if days <= 5:
+            ago_days = 0
+        else:
+            for i in range(days-5):
+                cond_1 = self.data.close[-i] < self.ma_fast[-i]    # 当日收盘低于fast线
+                cond_2 = min(pd.Series(self.data.close.get(ago=-i-1, size=5)) - pd.Series(self.ma_fast.get(ago=-i-1, size=5))) >= 0    # 前5日收盘均高于fast线
+                if cond_1 and cond_2:
+                    ago_days = i+1
+                    break
+        return ago_days
+
+    # 检查均线发散条件
     def checkDiv(self, ago=0, size=5):
         fast_minus_month = pd.Series(self.ma_fast.get(ago=ago, size=size)) - pd.Series(self.ma_month.get(ago=ago, size=size))
         cond_1 = fast_minus_month.equals(fast_minus_month.sort_values())
@@ -91,50 +159,62 @@ class SimpleStrategy(bt.Strategy):
         else:
             return False
 
+
     def next(self):
         # Check if an order is pending ... if yes, we cannot send a 2nd one
         if self.order:
             return
 
         if not self.position:
-            cond_1_1 = min(pd.Series(self.ma_fast.get(size=10)) - pd.Series(self.ma_month.get(size=10))) > 0    # fast线连续15日高于月线
-            cond_1_2 = min(pd.Series(self.ma_month.get(size=5)) - pd.Series(self.ma_quarter.get(size=5))) > 0    # 月线连续10日高于季度线
-            cond_1_3 = min(pd.Series(self.ma_quarter.get(size=3)) - pd.Series(self.ma_year.get(size=3))) > 0    # 月线连续10日高于季度线
+            # cond_1_1 = min(pd.Series(self.ma_fast.get(size=15)) - pd.Series(self.ma_month.get(size=15))) > 0    # fast线连续15日高于月线
+            # # cond_1_2 = min(pd.Series(self.ma_month.get(size=10)) - pd.Series(self.ma_quarter.get(size=10))) > 0    # 月线连续10日高于季度线
+            # cond_1_2 = ((list(self.ma_quarter.get(size=5))) == sorted(list(self.ma_quarter.get(size=5))))
+            # month_minus_quarter = pd.Series(self.ma_month.get(size=10)) - pd.Series(self.ma_quarter.get(size=10))
+            # cond_1_3 = month_minus_quarter.equals(month_minus_quarter.sort_values())
+            # cond_1_4 = self.ma_quarter[0] > self.ma_year[0]    # 季度线高于年线
+            # cond_1 = cond_1_1 and cond_1_2 and cond_1_3 and cond_1_4
+            # cond_2 = (self.data.close[0] > self.ma_fast[0]) and (self.ma_fast[-1] > self.data.close[-1] > self.ma_quarter[-1])    # 收盘价突破fast线
+            # cond_3 = self.data.close[0] > self.data.open[0]    # 当日收红
+            # if (cond_1 and cond_2 and cond_3):
+            #     days_count = self.findDDay()
+            #     # 自从趋势形成K线始终运行于fast线上方(只做第一次回调)
+            #     cond_4 = (self.countDrawdowns(days_count) == 1)
+            #     if cond_4:
+            #         self.order = self.buy()
+            cond_1_1 = min(pd.Series(self.ma_fast.get(size=10)) - pd.Series(self.ma_month.get(size=10))) > 0    # fast线连续10日高于月线
+            # cond_1_2 = min(pd.Series(self.ma_month.get(size=5)) - pd.Series(self.ma_quarter.get(size=5))) > 0    # 月线连续5日高于季度线
+            trend_days = self.findDDay()
+            ago_days = self.findDrawdownDay(trend_days)
+            month_minus_quarter = pd.Series(self.ma_month.get(ago=ago_days, size=9)) - pd.Series(self.ma_quarter.get(ago=ago_days, size=9))
+            cond_1_2 = month_minus_quarter.equals(month_minus_quarter.sort_values())
+            cond_1_3 = min(pd.Series(self.ma_quarter.get(size=3)) - pd.Series(self.ma_year.get(size=3))) > 0    # 季度线连续3日高于年线
             cond_1 = cond_1_1 and cond_1_2 and cond_1_3
             cond_2 = (self.data.close[0] > self.ma_fast[0]) and (self.ma_fast[-1] > self.data.close[-1] > self.ma_quarter[-1])    # 收盘价突破fast线
             cond_3 = self.data.close[0] > self.data.open[0]    # 当日收红
             if (cond_1 and cond_2 and cond_3):
                 self.order = self.buy()
+                # days_count = self.findDDay()
+                # # 自从趋势形成K线始终运行于fast线上方(只做第一次回调)
+                # cond_4 = (self.countDrawdowns(days_count) == 1)
+                # if cond_4:
+                #     self.order = self.buy()
             else:
                 pass
         else:
-            self.stopprice = max(0.9*self.data.high[0], self.stopprice)
-            # cond_1_1 = (self.data.high[0] > 1.1*self.buyprice and self.data.close[0] < self.ma_quarter[0])    # 止盈
-            # cond_1_2 = (self.data.close[-1] > 1.1*self.buyprice and self.data.close[0] < 1.1*self.buyprice)
-            # cond_1 = cond_1_1 or cond_1_2
-            # cond_1 = self.data.close[0] < self.stopprice
-            cond_1 = self.checkDiv(ago=-1, size=5) and not self.checkDiv(ago=0, size=5)    # 若近五日发散趋势结束，止盈
-            cond_2 = self.data.close[0] < self.ma_month[0]    # 止损
+            # self.stopprice = max(0.9*self.data.high[0], self.stopprice)
+            cond_1 = self.checkDiv(ago=-1, size=10) and not self.checkDiv(ago=0, size=10)    # 若近五日发散趋势结束，止盈
+            cond_2 = self.data.close[0] < self.ma_fast[0]    # 止损
             if (cond_1 or cond_2):
                 self.close()
 
 
 if __name__ == '__main__':
-    code_list = ['002642', '603019', '000617', '000802', '002563', '002076', '603111', '002245', '600562', '002070',
-                 '002426', '600628', '603977', '603888', '600385', '000026', '603499', '000603', '600298', '600117',
-                 '002674', '603912', '002222', '000692', '002728', '600701', '002919', '603199', '600513', '600543',
-                 '603589', '002952', '603558', '000561', '000811', '000933', '603318', '000962', '603920', '600268',
-                 '600857', '600352', '600594', '601877', '002196', '002675', '002552', '603385', '002272', '603833',
-                 '002641', '002597', '600330', '600843', '000711', '601633', '002850', '000828', '600909', '002052',
-                 '600869', '603617', '000709', '600830', '600826', '002568', '601599', '603639', '002292', '603113',
-                 '600188', '600695', '002400', '002499', '600210', '002275', '600269', '601609', '002334', '000868',
-                 '002553', '002475', '002305', '600828', '603722', '601015', '002909', '002100', '600510', '002101',
-                 '000712', '603918', '002905', '600487', '603688', '600415', '000333', '603081', '002261', '600232']
+    code_list = ['600088', '000423', '603013', '600612', '600707', '603696', '600572', '603088', '002173', '603990', '000723', '600532', '600499', '002364', '603311', '002966', '600569', '002424', '600792', '603893', '600876', '603488', '603639', '000851', '600668', '000034', '601100', '002699', '603970', '000823', '002248', '600444', '002692', '600368', '600608', '603027', '603305', '000426', '000078', '000963', '600012', '002428', '603698', '000883', '603399', '600463', '603818', '000736', '002907', '600076', '002564', '600738', '600380', '002288', '002009', '000509', '002092', '002408', '002698', '600586', '002795', '600308', '600798', '002091', '002014', '600747', '600099', '002676', '603348', '600789', '600745', '002338', '002660', '600626', '002865', '600637', '603880', '600061', '600897', '000050', '002937', '601233', '002971', '002182', '000650', '600161', '600279', '002712', '603618', '002779', '002777', '002678', '603656', '002623', '600984', '603519', '601101', '002631', '603799', '000759']
     total_profit = 0
     total_loss = 0
     win_num = 0
     loss_num = 0
-    # code_list = ['000692']
+    # code_list = ['603111']
     for code in code_list:
         stock_data = get_data(code, '20120610', '20220610')
         # print(stock_data.head(600).to_string())
@@ -166,7 +246,7 @@ if __name__ == '__main__':
     win_rate = win_num/(win_num + loss_num)
     print('Average profit: %.2f' % (average_profit))
     print('Average loss: %.2f' % (average_loss))
-    print('Overall average loss: %.2f' % (overall_average_profit))
+    print('Overall average profit: %.2f' % (overall_average_profit))
     print('Win rate: %.2f' % (win_rate))
     print('Decision value: %.2f' % (-average_profit/average_loss*win_rate))
 
